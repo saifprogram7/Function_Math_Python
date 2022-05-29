8aabeee4f34d40f07d773245f42aeb9d2df9acd0
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-05-29 11:57:54 +0100

    Add files via upload

---
+++ function-math.py
@@ -0,0 +1,16 @@
+#Using Function to Add 2 Values
+def additionmath(addnumberone, addnumbertwo):
+    print(addnumberone + addnumbertwo)
+additionmath(7, 3)
+#Using Function to Substract 2 Values
+def minusmath(minusnumberone, minusnumbertwo):
+    print(minusnumberone - minusnumbertwo)
+minusmath(7, 3)
+#Using Function to Multiply 2 Values
+def multiplymath(multiplynumberone, multiplynumbertwo):
+    print(multiplynumberone * multiplynumbertwo)
+multiplymath(7, 3)
+#Using Function to Divide 2 Values
+def dividemath(dividefirstnumber, dividesecondnumber):
+    print(round(dividefirstnumber / dividesecondnumber))
+dividemath(8, 2)
